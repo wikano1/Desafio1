91c356239ce9b1caa756dac4af96cddff2c9a6b1
AriesAli <ariesalisosa@gmail.com>
2024-03-17 11:32:59 -0300

    Corregido

--- ProductManager.js
+++ ProductManager.js
@@ -1,25 +1,22 @@
-
-
 class ProductManager {
     #products;
     static idProducto = 0;
 
     constructor() {
-        this.products = [];
+        this.#products = [];
     }
 
-    addProductos(title, description, price, thumbnail, code, stock) {
+    addProduct(title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || !stock)
             return "Todos los parametros son requeridos ";
 
         const codeRepetido = this.#products.some(p => p.code == code);
         if (codeRepetido)
-            return "Ya se encuentra registrado el codigo";
-
+            return `Ya se encuentra registrado el codigo ${code} `;
 
         ProductManager.idProducto = ProductManager.idProducto + 1
-        const id = ProductManager.idProducto + 1;
-        producto = nuevoProducto = {
+        const id = ProductManager.idProducto;
+        const nuevoProducto = {
             id: id,
             title: title,
             description: description,
@@ -28,29 +25,22 @@ class ProductManager {
             code: code,
             stock: stock
         };
-        //ProductManager.idProducto++;
         this.#products.push(nuevoProducto);
 
         return "Producto Agreagado"
-
     }
 
     getProducts() {
-        return this.products;
-
+        return this.#products;
     }
 
     getProductsById(id) {
-        const producto = this.products.find(p => p.id == id)
-        if (producto)
-            return producto;
+        const product = this.#products.find(p => p.id == id);
+        if (product)
+            return product;
         else
             throw new Error('No existe un producto con ese ID');
-
     }
-
 }
-
-
 
 module.exports = ProductManager;
--- testing.js
+++ testing.js
@@ -2,13 +2,8 @@ const ProductManager = require("./ProductManager")
 
 const producto = new ProductManager();
 
+console.log(producto.addProduct("Carro", "Chevrolet 01", 5000, "https://imagen.com", "asda23", 4000))
+console.log(producto.addProduct("moto", "honda 01", 3000, "https://imagen.com", "rrr4", 1000))
 
-
-console.log(producto.getProducts());
-//console.log(producto.getProductsById())
-
-console.log(producto.addProducts("Carro", "Chevrolet 01", 5000, "https://imagen.com", "asda23", 4000))
-console.log(producto.addProducts("Carro", "Chevrolet 01", 5000, "https://imagen.com", "asda23", 4000))
-
-
+console.log(producto.getProducts())
 
